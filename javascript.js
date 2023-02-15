cea6c5514612da0212dc24b57d97076d3315aab1
rymurray1 <ryanmmurray8@gmail.com>
2023-02-15 17:43:13 -0600

    Create initial file and outline algorithm

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* 
+    - Take computer input as a number
+    - Translate number into rock, paper, or scissors
+    - Take user input as a string
+    - Convert string to match style choice for comparison
+    - State user and computer choices, and declare winner of game
+    - Record winner by increasing record counter for winner by 1
+    - Allow choice to reset the game
+    - When computer or user reaches five wins, declare overall winner
